d92533498bbbcaa8b9801b81438ddde9352b8d8e
alionurulker <alionurulker@hotmail.com>
2022-07-11 13:27:18 +0000

    BCD-3 Handled DB Connection Admin Panel

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 APPEND_SLASH = True
 
 ALLOWED_HOSTS = ["bestcardealership.eu-gb.mybluemix.net"]
-
+#ALLOWED_HOSTS = ["localhost"]
 
 # Application definition
 
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+#Postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'MzkzLWFsaXVsa2Vy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
